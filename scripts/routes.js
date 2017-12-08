9633597929a70c0dc63a77a5170328dc21621129
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-12-08 15:58:26 -0800

    adds routes.js

---
+++ scripts/routes.js
@@ -0,0 +1,16 @@
+page('/api/v1/book-app', (ctx, next) => {
+    app.Book.fetchAll(app.bookView.initIndexPage);
+});
+
+page('/books/:id', app.Book.fetchOne, app.bookView.initDetailPage);
+
+page('/new', app.newView.initNewPage);
+
+page('/about', app.aboutView.initAboutPage);
+
+page('*', (ctx, next) => { 
+    console.log(ctx);
+    console.log('Nothing to see here!'); 
+});
+ 
+page.start();
