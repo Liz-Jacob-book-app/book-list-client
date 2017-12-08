e2ec3290dd12e35aaf35c218c411d190fb162261
pereztjacob <pereztjacob@gmail.com>
2017-12-07 16:41:24 -0800

    creates Book object and links it to heroku db

--- scripts/books.js
+++ scripts/books.js
@@ -1,16 +1,21 @@
-function(module
-    )
-    function Book
-
+(function(module){
+    function Book(obj){
+        this.book_id = obj.book_id,
+        this.title = obj.title,
+        this.author = obj.author,
+        this.isbn = obj.isbn,
+        this.image_url = obj.imageUrl,
+        this.description = obj.description
+    }
+    
     Book.all = [];
 
     Book.fetchAll = () => {
-        $.get('heroku api')
-            .done('to do with it')
+        $.get('https://book-app-lj.herokuapp.com/api/v1/books/')
+        .done(console.log);
     }
 
     module.Book = Book;
+})(app);
 
-(function(module){
-    function Book = 
-})
+app.Book.fetchAll();
