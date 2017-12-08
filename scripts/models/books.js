c0297818c401572b8f920aed4526f385076b6c24
pereztjacob <pereztjacob@gmail.com>
2017-12-08 14:44:29 -0800

    developed js files

--- scripts/models/books.js
+++ scripts/models/books.js
@@ -7,15 +7,24 @@
         this.author = obj.author,
         this.isbn = obj.isbn,
         this.image_url = obj.imageUrl,
-        this.description = obj.description
+        this.description = obj.description;
     }
-    
+
     Book.all = [];
 
     Book.fetchAll = () => {
         $.get('https://book-app-lj.herokuapp.com/api/v1/books/')
-        .done(console.log);
-    }
+            .done(Book.loadAll);
+    };
+
+    Book.loadAll = (data) => {
+        Book.all = data.map(obj => new Book(obj));
+    };
+
+    Book.prototype.toHtml = function () {
+        const fillTemplate = Handlebars.compile($('#books-template').text());
+        return fillTemplate(this);
+    };
 
     module.Book = Book;
 })(app);
