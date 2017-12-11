45a9017ee4c70793f917a0a5ee2fb89b7b3ce6eb
pereztjacob <pereztjacob@gmail.com>
2017-12-11 15:50:39 -0800

    allows user to create book

--- scripts/models/books.js
+++ scripts/models/books.js
@@ -1,8 +1,8 @@
 'use strict';
 var app = app || {};
 
-const API_URL = 'https://book-app-lj.herokuapp.com/api/v1/books';
-// const API_URL = 'https://localhost:5000';
+// const API_URL = 'https://book-app-lj.herokuapp.com/api/v1/books';
+const API_URL = 'http://localhost:5000/api/v1/books';
 
 (function(module){
     function Book(obj){
@@ -16,15 +16,20 @@ const API_URL = 'https://book-app-lj.herokuapp.com/api/v1/books';
 
     Book.all = [];
 
-    Book.update = (id, data) => {
-        $.ajax({
-            url: `${API_URL}/api/v1/books/${id}`,
-            method: 'PUT',
-            data: data
-        })
-            .then(data => {
-                page(`/books/:{id}`);
-            });
+    // Book.update = (id, data) => {
+    //     $.ajax({
+    //         url: `${API_URL}/api/v1/books/${id}`,
+    //         method: 'PUT',
+    //         data: data
+    //     })
+    //         .then(data => {
+    //             page(`/books/:{id}`);
+    //         });
+    // };
+
+    Book.create = newBook => {
+        $.post(`${API_URL}`, newBook)
+            .then(console.log);
     };
 
     Book.fetchOne = (ctx, cb) => {
@@ -35,7 +40,6 @@ const API_URL = 'https://book-app-lj.herokuapp.com/api/v1/books';
             })
             .fail(console.error);
     };
-
     Book.fetchAll = (cb) => {
         $.get(`${API_URL}`)
             .then(Book.loadAll)
@@ -44,7 +48,6 @@ const API_URL = 'https://book-app-lj.herokuapp.com/api/v1/books';
     Book.loadAll = (data) => {
         Book.all = data.map(obj => new Book(obj));
     };
-
     Book.prototype.toHtml = function () {
         const fillTemplate = Handlebars.compile($('#books-template').text());
         return fillTemplate(this);
--- scripts/views/bookView.js
+++ scripts/views/bookView.js
@@ -6,6 +6,7 @@ var app = app || {};
     const bookView = {};
 
     bookView.initIndexPage = () => {
+        // $('#add').hide();
         $('main section').hide();
         $('#books').empty().show();
         console.log(app.Book.all);
@@ -18,19 +19,34 @@ var app = app || {};
         $('#books').append(ctx.book.toHtml());
     };
 
-    bookView.initUpdatePage = (ctx) => {
+    bookView.initNewPage = (ctx) => {
+        event.preventDefault();
         $('main section').hide();
-        $('#update-book').parent().show();
+        $('#add').show();
 
-        $('#update-book input[name="author"]').val(ctx.book.author);
-
-        $('#update-book').on('submit', function(){
+        $('#add').on('submit', function(){
             event.preventDefault();
-            const updatedData = {
-                author: $('#update-book input[name="author"].val()')
+            console.log('asdfs');
+            const newBook = {
+                title: $('#title').val(),
+                author: $('#author').val(),
+                isbn: $('#isbn').val(),
+                imageUrl: $('#image_url').val(),
+                description: $('#description').val()
             };
-            app.Book.update(ctx.book.id, updatedData);
+            console.log(newBook);
+            app.Book.create(newBook);
         });
+
+        // $('#update-book input[name="author"]').val(ctx.book.author);
+
+        // $('#update-book').on('submit', function(){
+        //     event.preventDefault();
+        //     const updatedData = {
+        //         author: $('#update-book input[name="author"].val()')
+        //     };
+        //     app.Book.update(ctx.book.id, updatedData);
+        // });
     };
 
     module.bookView = bookView;
