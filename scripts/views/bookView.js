5a5faf848dd785f163e17e92c9097e0b82a14a24
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-12-11 14:11:31 -0800

    pushes form and other updates to publicjs

--- scripts/views/bookView.js
+++ scripts/views/bookView.js
@@ -18,46 +18,20 @@ var app = app || {};
         $('#books').append(ctx.book.toHtml());
     };
 
-    bookView.initNewBookPage = (ctx) => {
-        $('.main section').show();
-        $('#form').hide();
-        $('#book-json').on('focus', function(){
-            this.select();
+    bookView.initNewBookPage = () => {
+        $('.main section').hide();
+        $('#add').parent().show();
+
+        $('#add').on('submit', function() {
+            event.preventDefault();
+            const newBook = {
+                title: this.title.value,
+                author: this.author.value,
+                isbn: this.isbn.value,
+                imageUrl: this.imageUrl.value,
+                description: this.description.value
+            };
         });
-
-        $('#new-form').on('change', 'input, textarea', bookView.create);
-        $('#new-form').on('submit', bookView.submit);
     };
-
-    bookView.create = () => {
-        let addBook;
-        $('#add').empty();
-
-        addBook = new app.Add({
-            title: $('#title').val(),
-            author: $('#author').val(),
-            isbn: $('#isbn').val(),
-            imageUrl: $('#image_url').val(),
-            description: $('#description').val(),
-        });
-
-        $('#add').append(newBook.toHtml());
-    };
-
-    bookView.submit = event => {
-        event.preventDefault();
-        const book = new app.Book({
-            title: $('#title').val(),
-            author: $('#author').val(),
-            isbn: $('#isbn').val(),
-            imageUrl: $('#image-url').val(),
-            description: $('#desc').val()
-        });
-
-        book.insertRecord();
-
-        window.location = '../';
-    };
-
     module.bookView = bookView;
 })(app);
